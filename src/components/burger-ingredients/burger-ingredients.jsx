e93613608e3285333abc3b0b717bd4eec26e65e3
Olga Kopyova <imkopyova@gmail.com>
2024-04-15 11:29:22 +0300

    refactor: Доработать компоненты

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import { IngredientCard } from './ingredient-card/ingredient-card';
 import { IngredientsSection } from './ingredients-section/ingredients-section';
+import { Scrollable } from '../scrollable/scrollable';
 import styles from './burger-ingredients.module.css';
 import { DATA } from '../../utils/data';
+
+const FIXED_HEIGHT_WITHOUT_SCROLLABLE = 244;
 
 export const BurgerIngredients = () => {
     const [current, setCurrent] = React.useState('bun');
@@ -12,7 +15,7 @@ export const BurgerIngredients = () => {
     const sauces = DATA.filter(ingredient => ingredient.type === 'sauce');
     const mains = DATA.filter(ingredient => ingredient.type === 'main');
     return (
-        <div className={styles.container}>
+        <section className={styles.container}>
             <div className={styles.tabs}>
                 <Tab
                     value="bun"
@@ -36,39 +39,47 @@ export const BurgerIngredients = () => {
                     Начинки
                 </Tab>
             </div>
-            <IngredientsSection name="Булки">
-                {buns.map(ingredient => (
-                    <IngredientCard
-                        key={ingredient._id}
-                        name={ingredient.name}
-                        price={ingredient.price}
-                        image={ingredient.image}
-                        quantity={1}
-                    />
-                ))}
-            </IngredientsSection>
+            <Scrollable
+                availableHeight={
+                    window.outerHeight - FIXED_HEIGHT_WITHOUT_SCROLLABLE
+                }
+            >
+                <div>
+                    <IngredientsSection name="Булки">
+                        {buns.map(ingredient => (
+                            <IngredientCard
+                                key={ingredient._id}
+                                name={ingredient.name}
+                                price={ingredient.price}
+                                image={ingredient.image}
+                                quantity={1}
+                            />
+                        ))}
+                    </IngredientsSection>
 
-            <IngredientsSection name="Соусы">
-                {sauces.map(ingredient => (
-                    <IngredientCard
-                        key={ingredient._id}
-                        name={ingredient.name}
-                        price={ingredient.price}
-                        image={ingredient.image}
-                    />
-                ))}
-            </IngredientsSection>
+                    <IngredientsSection name="Соусы">
+                        {sauces.map(ingredient => (
+                            <IngredientCard
+                                key={ingredient._id}
+                                name={ingredient.name}
+                                price={ingredient.price}
+                                image={ingredient.image}
+                            />
+                        ))}
+                    </IngredientsSection>
 
-            <IngredientsSection name="Начинки">
-                {mains.map(ingredient => (
-                    <IngredientCard
-                        key={ingredient._id}
-                        name={ingredient.name}
-                        price={ingredient.price}
-                        image={ingredient.image}
-                    />
-                ))}
-            </IngredientsSection>
-        </div>
+                    <IngredientsSection name="Начинки">
+                        {mains.map(ingredient => (
+                            <IngredientCard
+                                key={ingredient._id}
+                                name={ingredient.name}
+                                price={ingredient.price}
+                                image={ingredient.image}
+                            />
+                        ))}
+                    </IngredientsSection>
+                </div>
+            </Scrollable>
+        </section>
     );
 };
--- src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
@@ -1,17 +1,14 @@
 import classNames from 'classnames';
-import {
-    Counter,
-    CurrencyIcon,
-} from '@ya.praktikum/react-developer-burger-ui-components';
+import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './ingredient-card.module.css';
+import { Price } from '../../price/price';
 
 export const IngredientCard = ({ name, price, image, quantity }) => {
     return (
         <div className={classNames(styles.card, 'pr-4 pl-4')}>
             <img width={240} height={120} alt={name} src={image} />
-            <div className={classNames('mt-1', styles.price)}>
-                <span className="text text_type_digits-default">{price}</span>
-                <CurrencyIcon type="primary" />
+            <div className="mt-1">
+                <Price>{price}</Price>
             </div>
             <h3
                 className={classNames(
