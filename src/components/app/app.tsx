f126746b27e81787f74e5dceb0b2f65a4a8f956f
Olga Kopyova <imkopyova@gmail.com>
2024-05-19 20:16:57 +0300

    feat: Подключить react-router-dom

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { AppHeader } from '../app-header/app-header';
+import { HomePage } from '../../pages/home/home';
 
 import styles from './app.module.css';
 
@@ -6,6 +9,11 @@ export const App = () => {
     return (
         <div className={styles.app}>
             <AppHeader />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </Router>
         </div>
     );
 };
